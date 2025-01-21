1948f312c256239333b5a2340353d91be07ddb04
ftrugon <fjtrujillo@iesrafaelalberti.es>
2025-01-21 20:31:56 +0100

    pillar palabra aleatoria hecho, fala hashearla y hacer que 1 hilo pueda hacer la funcion para recorrer la lista

--- PSP_PasswordCracker/Program.cs
+++ PSP_PasswordCracker/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Pass the file path and file name to the StreamReader constructor
+String filePath = "C:\\Users\\fran\\RiderProjects\\HilosGenericos\\HilosGenericos\\10k-most-common.txt";
+//Read the first line of text
+
+string[] lines = File.ReadAllLines(filePath);
+Random rng = new Random();
+int randomNumber = rng.Next(0, lines.Length);
+Console.WriteLine(lines[randomNumber]);
+
+Console.ReadLine();
