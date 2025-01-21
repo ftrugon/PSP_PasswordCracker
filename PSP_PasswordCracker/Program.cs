e26747260f996d85feddd5ade6ea45b544e2d0f2
ftrugon <fjtrujillo@iesrafaelalberti.es>
2025-01-21 21:05:04 +0100

    hecho la parte 1

--- PSP_PasswordCracker/Program.cs
+++ PSP_PasswordCracker/Program.cs
@@ -2,32 +2,62 @@
 using System.Security.Cryptography;
 using System.Text;
 
-String filePath = "C:\\Users\\fran\\RiderProjects\\HilosGenericos\\HilosGenericos\\10k-most-common.txt";
 
-string[] lines = File.ReadAllLines(filePath);
-Random rng = new Random();
-int randomNumber = rng.Next(0, lines.Length);
-String tal = lines[randomNumber];
-
-Console.WriteLine(HashString(tal));
-
-
-
-String HashString(String toHash)
+class Program
 {
-    byte[] tmpSource = ASCIIEncoding.ASCII.GetBytes(toHash);
-    return ByteArrayToString(new MD5CryptoServiceProvider().ComputeHash(tmpSource));
     
-}
-
-
-static string ByteArrayToString(byte[] arrInput)
-{
-    int i;
-    StringBuilder sOutput = new StringBuilder(arrInput.Length);
-    for (i=0;i < arrInput.Length; i++)
+    static void Main(string[] args)
     {
-        sOutput.Append(arrInput[i].ToString("X2"));
+        String filePath = "C:\\Users\\fran\\RiderProjects\\HilosGenericos\\HilosGenericos\\10k-most-common.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+        Random rng = new Random();
+        int randomNumber = rng.Next(0, lines.Length);
+        
+        String tal = lines[randomNumber];
+        String hash = HashString(tal);
+        Console.WriteLine($"PALABRA A DESCIFRAR --> {tal}, HASH --> {hash}");
+        
+        
+        Thread hilo = new Thread(() => CheckPasswords(lines, hash));
+        hilo.Start();
     }
-    return sOutput.ToString();
+
+    static String HashString(String toHash)
+    {
+        byte[] tmpSource = ASCIIEncoding.ASCII.GetBytes(toHash);
+        return ByteArrayToString(new MD5CryptoServiceProvider().ComputeHash(tmpSource));
+    
+    }
+
+
+    static string ByteArrayToString(byte[] arrInput)
+    {
+        int i;
+        StringBuilder sOutput = new StringBuilder(arrInput.Length);
+        for (i=0;i < arrInput.Length; i++)
+        {
+            sOutput.Append(arrInput[i].ToString("X2"));
+        }
+        return sOutput.ToString();
+    }
+
+    static void CheckPasswords(String[] passwords,String correctHash)
+    {
+        foreach (var pass in passwords)
+        {
+            if (HashString(pass) == correctHash)
+            {
+                Console.WriteLine("Passwords Match");
+                Console.WriteLine("---------------");
+                Console.WriteLine($"{pass} --> {HashString(pass)}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Passwords Not Match");
+            }
+        }
+    }
+    
 }
